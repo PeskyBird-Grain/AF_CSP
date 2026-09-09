1cb5922c6b9d7d3658ee4ab08a41fbe32f08376e
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-09 13:52:10 -0600

    variable notes

---
+++ variables_notes.py
@@ -0,0 +1,7 @@
+# AF, Variables Notes
+
+name = input("Tell me your name: ")
+
+print("CSP is the best class!")
+print("Why? you may ask. Because... yea.")
+print("Hello", name, "you fell directly into my trap... Mwa ha ha!!")
