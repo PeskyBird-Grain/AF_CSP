3d813d2d67cdcf872c07d077fc3478d1ec826451
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-10 13:33:59 -0600

    variables practice

---
+++ variables_practice.py
@@ -0,0 +1,14 @@
+# AF, Variables Practice
+
+name = input("Name: ")
+small_number = input("Number 1-10: ")
+big_number = input("Number between 100 and 1000: ")
+breakfast = input("What you had for breakfast: ")
+color = input("Favorite color: ")
+school = input("Name of a school: ")
+year = input("Year: ")
+eye = input("Your eye color: ")
+age = input("Someone's age: ")
+subject = input("Favorite subject: ")
+
+print(name, small_number, big_number, breakfast, color, school, year, eye, age, subject)
