4f371b8d78d5b31ce392694a4e9402222b29b2e0
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-11 13:35:45 -0600

    physical and logical memory notes

---
+++ physical_logical_memory.py
@@ -0,0 +1,8 @@
+# AF, Physical and Logical Memory
+
+pi = 3.141592653589793
+
+everything = 42
+
+print(f"The variable pi is {pi} and it is saved at {id(pi)}")
+print(f"the variable everything is {everything} and it is saved at {id(everything)}")
