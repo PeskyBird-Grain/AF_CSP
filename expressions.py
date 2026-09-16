46d9d387d5d9b1d20a2916e7c1834b17f4b18670
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-16 13:53:12 -0600

    expressions notes

---
+++ expressions.py
@@ -0,0 +1,28 @@
+# AF, Integers, Floats, and Expressions
+
+# integers
+people = 18
+
+shoes = people * 2
+
+hand = people * 2
+
+# Float
+pi = 3.14
+
+water = 60.5
+
+temp = 68.8
+
+money = 1.99
+
+# Expression
+print(f"Your {water} gallons of water cost ${round(water*money,2)}")
+
+# Modulo and Integer Division
+print(f"18/4= {18/4} which is {18//4} with a remainder of {18%4}")
+print(f"18/5= {18/5} which is {18//5} with a remainder of {18%5}")
+print(f"18/6= {18/6} which is {18//6} with a remainder of {18%6}")
+print(f"18/7= {18/7} which is {18//7} with a remainder of {18%7}")
+
+print(int(shoes*(5-1)/3))
