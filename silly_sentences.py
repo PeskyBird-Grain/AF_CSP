eaee1124cf298e356d3dd8119a82e8d4091bf8c2
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-21 13:53:13 -0600

    silly sentences

---
+++ silly_sentences.py
@@ -0,0 +1,12 @@
+# AF, Silly Sentences
+
+print("This is a madlibs.\nPlease follow the instructions.\n\nKEEP INPUTS SCHOOL APPROPRIATE!!\n")
+
+first = input("Tell me an adjective: ").strip().lower()
+second = input("Tell me a building: ").strip()
+third = input("Tell me a feature of a house: ").strip().lower()
+fourth = input("Tell me a natural disaster: ").strip().lower()
+fifth = input("Tell me an exclamation: ").strip()
+sixth = input("Tell me an adjective: ").strip().lower()
+seventh = input("Tell me a vehicle: ").strip().lower()
+print("\nI was sitting in my " + first + " " + second + ", staring out of the " + third + " at the " + fourth + ". " + '"' + fifth + '!"' + "I exclaimed as a/an " + sixth + " " + seventh + " was hurled past me.")
