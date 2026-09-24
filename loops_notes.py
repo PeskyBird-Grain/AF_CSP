e8fbd9763a698f065580dc2fa28485ccb0f8d5fe
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-24 13:51:53 -0600

    loops notes

---
+++ loops_notes.py
@@ -0,0 +1,21 @@
+# AF, Loops Notes
+import random
+count = 1
+
+while count <= 10:
+    print(count)
+    count += 1
+
+
+goose = random.randint(1,11)
+ducks = 1
+
+while True:
+    print("Duck")
+    if ducks == goose:
+        break
+    ducks += 1
+print("GOOSE!!!!!")
+
+
+siblings = ["Billy", "Bob", "Joe", "Alex"]
