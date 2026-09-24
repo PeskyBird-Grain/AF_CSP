32d7359b7f86de0926e8314a7deefb85d6336d23
PeskyBird-Grain <alex.fulton@ucas-edu.net>
2026-09-24 13:17:38 -0600

    password thing

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -1,4 +1,10 @@
 # AF, Password Strength Checker
+one = " "
+two = " "
+three = " "
+four = " "
+five = " "
+length = "False"
 up = "False"
 low = "False"
 num = "False"
@@ -7,8 +13,6 @@ count = 0
 password = input("What is your password: ")
 if len(password) >= 8:
     length = "True"
-else:
-    length = "False"
 for letter in password:
     if letter.isupper():
         up = "True"
@@ -18,5 +22,36 @@ for letter in password:
         num = "True"
     if letter in "!@#$%^&*()_-+=<>?/.,;:[}{]`~":
         symbol = "True"
-    if up == "True":
-        count = count + 1
+if length == "True":
+    count = count + 1
+if up == "True":
+    count = count + 1
+if low == "True":
+    count = count + 1
+if num == "True":
+    count = count + 1
+if symbol == "True":
+    count = count + 1
+if length == "False":
+    one = "8 characters long"
+if up == "False":
+    two = "Add uppercase"
+if low == "False":
+    three = "Add lowercase"
+if num == "False":
+    four = "Add number"
+if symbol == "False":
+    five = "Add a symbol"
+if count <=2:
+    strength = "Weak"
+if count == 4 or count == 3:
+    strength = "Medium"
+if count == 5:
+    strength = "Strong"
+print("At least 8 characters: " + length)
+print("Has an uppercase letter: " + up)
+print("Has a lowercase letter: " + low)
+print("Has a number: " + num)
+print("Has a symbol: " + symbol)
+print("Your password strength: " + strength)
+print("To make it strong: " + one + two + three + four + five)
